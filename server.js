5874ba0c1c30f916cb62300269af808aa45e33fb
Sayantan <sayanbis83@gmail.com>
2019-01-29 23:45:25 +0530

    detects urls to shorten

--- server.js
+++ server.js
@@ -12,12 +12,20 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI);
+var u = new mongoose.Schema({
+  short: Number,
+  orig: String
+});
+var url = mongoose.model('url',u);
+var count = 1;
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+var bodyparser = require('body-parser');
+app.use(bodyparser.urlencoded({extended: false}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -27,8 +35,28 @@ app.get('/', function(req, res){
 
   
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.post("/api/shorturl/new", function (req, res,done) {
+  var orig_url = req.body.url;
+  console.log(orig_url);
+  url.findOne({orig: orig_url}, function(err,data){
+    if (err) done(err);
+    if(data == null){
+      var document= new url({short: count, orig: orig_url});
+      document.save(function(err,data){
+        if (err) done(err);
+        done(null, data);
+      });
+      res.send({original_url: orig_url, short_url: count});
+      count++;
+      done(null, data);
+    }
+    else
+    {
+      res.send({original_url: orig_url, short_url: data.short});
+    }
+  });
+  // console.log(req.params.url);
+  // res.json({greeting: 'hello API'});
 });
 
 
